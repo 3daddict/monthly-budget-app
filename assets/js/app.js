13a8a6a98ccb7e6dba638ef3ebca2dee8231fb85
Michael Salvati <print@3daddict.com>
2018-11-27 13:54:31 -0800

    uicontroller init setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,53 @@
+
+//Budget Controller
 const budgetController = (function() {
-
-    let x = 23;
-
-    let add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
-})();
-
-const UIController = (function() {
 
     //code
 
 })();
 
+// UI Controller
+const UIController = (function() {
+
+    return {
+		getInput: function(){
+			return {
+				type: document.querySelector('.add__type').value, //will be either inc (+) or exp (-)
+				desctiption: document.querySelector('.add__description').value,
+				value: document.querySelector('.add__value').value
+			}
+		}
+	};
+
+})();
+
+// Main App Controller
 const controller = (function(budgetCtrl, UICtrl) {
 
-   let z = budgetCtrl.publicTest(5);
+	const ctrlAddItem = function(){
+		//1. Get the field input data
+		let input = UICtrl.getInput();
+		console.log(input);
+		//2. Add item to the budget controller
 
-   return {
-       anotherPublic: function() {
-           console.log(z);
-       }
-   }
+		//3. Add the new item to the ui controller
+
+		//4. Calculate the budget
+
+		//5. display the budget on the ui
+
+	}
+
+	//input event listener
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+		if(event.keycode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+
+
+	});
 
 })(budgetController, UIController);
+
