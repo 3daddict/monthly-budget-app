f5402470cb2e54632569683c25b4020469e4f4d7
Michael Salvati <print@3daddict.com>
2018-11-28 16:08:15 -0800

    month and year function created and shown on DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -139,7 +139,8 @@ const UIController = (function() {
 		expenseLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercLabel: '.item__percentage'
+		expensesPercLabel: '.item__percentage',
+		dateLabel: '.budget__title--month'
 	};
 
 	const formatNumber = function(num, type) {
@@ -265,6 +266,17 @@ const UIController = (function() {
 			});
 		},
 
+		displayMonth: function() {
+			let now, year, month, months;
+			
+			now = new Date();
+			months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+			month = now.getMonth();
+			year = now.getFullYear();
+
+			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -349,6 +361,7 @@ const controller = (function(budgetCtrl, UICtrl) {
 	return {
 		init: function() {
 			console.log("App Ready");
+			UICtrl.displayMonth();
 			UICtrl.displayBudget({
 					budget: 0,
 					totalInc: 0,
